3965c48f9e29d500a77c7a0ef23e2c026bcec3ed
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-13 11:50:41 -0800

    Add intent to log data input

--- app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
@@ -1,13 +1,17 @@
 package com.freeman_smith.karen.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
             mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                    String location = mLocationEditText.getText().toString();
+                    Log.d(TAG, location);
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    startActivity(intent);
                 }
             });
     }
