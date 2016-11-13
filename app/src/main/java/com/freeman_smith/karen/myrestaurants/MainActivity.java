d7bcc960a5b23cbc1600d6eea40841f4ccc5a3e9
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-13 12:46:09 -0800

    show zip code on second view

--- app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
@@ -3,6 +3,7 @@ package com.freeman_smith.karen.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
             mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String location = mLocationEditText.getText().toString();
-                    Log.d(TAG, location);
                     Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    intent.putExtra("location", location);
                     startActivity(intent);
                 }
             });
