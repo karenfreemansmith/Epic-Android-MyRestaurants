1a2c76a4992bee679a4ad9eca37c49a67ced00b5
karenfreemansmith <karenfreemansmith@gmail.com>
2016-11-14 12:59:20 -0800

    pass android espresso tests

---
+++ app/src/androidTest/java/com/freeman_smith/karen/myrestaurants/MainActivityInstrumentationTest.java
@@ -0,0 +1,37 @@
+package com.freeman_smith.karen.myrestaurants;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Guest on 11/14/16.
+ */
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("12345"))
+                .check(matches(withText("12345")));
+    }
+
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String location = "12345";
+        onView(withId(R.id.locationEditText)).perform(typeText(location));
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches(withText("Here are all the restaurants near: " + location)));
+    }
+}
+
+
