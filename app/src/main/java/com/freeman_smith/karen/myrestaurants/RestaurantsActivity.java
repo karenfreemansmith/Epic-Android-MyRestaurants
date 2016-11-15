a659886f839c3f21cb97c76940e3b7b2a6f9dc4f
karenfreemansmith <karenfreemansmith@gmail.com>
2016-11-14 17:46:30 -0800

    add list adapter with item event listener and toast

--- app/src/main/java/com/freeman_smith/karen/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/RestaurantsActivity.java
@@ -1,19 +1,40 @@
 package com.freeman_smith.karen.myrestaurants;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private String[] restaurants = new String[] {"McDonalds", "Arby's", "Taco Bell", "Wendy's", "Del Taco", "Taco Time", "Subway", "Denny's", "IHOP", "Iron Skillet", "Dairy Queen"};
+    private ListView mListView;
     private TextView mLocationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
