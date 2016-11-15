506f2de2c4308be91d95ae12d60461d43b5d1c16
karenfreemansmith <karenfreemansmith@gmail.com>
2016-11-14 18:14:56 -0800

    refactor main activity with butterknife

--- app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/MainActivity.java
@@ -9,28 +9,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
-    private TextView mTitleTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTitleTextView = (TextView) findViewById(R.id.titleTextView);
+        ButterKnife.bind(this);
+
         Typeface philosopherFont = Typeface.createFromAsset(getAssets(), "fonts/Philosopher-Regular.ttf");
         mTitleTextView.setTypeface(philosopherFont);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-            mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String location = mLocationEditText.getText().toString();
-                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                }
-            });
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
