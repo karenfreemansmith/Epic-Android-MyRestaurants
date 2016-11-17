e3902d9959b6b76b018a632d9bec8bd70e57e013
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-17 12:41:00 -0800

    added an API key for Yelp

--- app/build/generated/source/buildConfig/androidTest/debug/com/freeman_smith/karen/myrestaurants/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/freeman_smith/karen/myrestaurants/test/BuildConfig.java
@@ -10,4 +10,9 @@ public final class BuildConfig {
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String YELP_CONSUMER_KEY = "eS8Wa9jypTdKkI-NuoKsWw";
+  public static final String YELP_CONSUMER_SECRET = "mMdIWU9lv3FhQWUePYXn3cqktfY0cXJD8xzeTsCJ4wi4ucHbCwnIiAPDDNEvqPyM";
+  public static final String YELP_TOKEN = "Bearer";
+  public static final String YELP_TOKEN_SECRET = ""w5ZJQjLQ38Vwc-R_8Zh8yr0pSHo6g9_NbjFULtmfkvcb7qMtPJVc5szGXvEY8XAM-7zN2ft-UKyCUzMd2vjuD8b5-07yDEX36czDMFLST-vBCMrcbMY_LUpzPxQuWHYx";
 }
