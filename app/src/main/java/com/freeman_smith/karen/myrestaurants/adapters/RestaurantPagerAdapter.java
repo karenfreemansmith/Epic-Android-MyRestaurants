1d6a8c3efb06a70e90620e254ecccc5a444dce57
Elysia Nason <elysia.avery@gmail.com>
2016-11-30 17:03:55 -0800

    add fragment and parceler

--- app/src/main/java/com/freeman_smith/karen/myrestaurants/adapters/RestaurantPagerAdapter.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/adapters/RestaurantPagerAdapter.java
@@ -1,7 +1,7 @@
 package com.freeman_smith.karen.myrestaurants.adapters;
 
-import android.app.FragmentManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.freeman_smith.karen.myrestaurants.models.Restaurant;
--- app/src/main/java/com/freeman_smith/karen/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/freeman_smith/karen/myrestaurants/models/Restaurant.java
@@ -19,6 +19,8 @@ public class Restaurant {
     private double mLongitude;
     private ArrayList<String> mCategories = new ArrayList<>();
 
+    public Restaurant() {}
+
     public Restaurant(String name, String phone, String website, double rating, String imageURL, ArrayList<String> address, double latitude, double longitude, ArrayList<String> categories) {
         this.mName = name;
         this.mPhone = phone;
